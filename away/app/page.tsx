0964f62ecb2077e7daae7e39a2f7afc88c5e629c
Matgiam <220775874+Matgiam@users.noreply.github.com>
2026-05-04 11:45:13 +0200

    feat: add background using react-bits Silk component

--- away/app/page.tsx
+++ away/app/page.tsx
@@ -7,6 +7,7 @@ import { useAudioEngine } from "../hooks/useAudioEngine";
 import { VisNote } from "../lib/types";
 import { Visualizer } from "@/components/Visualizer";
 import { Navigation } from "@/components/Navigation";
+import { SilkBackground } from "@/components/SilkBackground";
 
 export default function App() {
 	const [showKeys, setShowKeys] = useState(true);
@@ -20,9 +21,10 @@ export default function App() {
 		<div className="h-screen w-screen bg-[#050505] text-gray-200 overflow-hidden flex relative">
 			{showHomeScreen ? (
 				<>
+        					<SilkBackground color="#0b0416" scale={0.8} noiseIntensity={1.3} speed={3} rotation={180} />
+
 					<div className="absolute inset-0 z-10">
-								<div className="absolute bottom-0 left-0 right-0 h-32">
-						</div>
+						<div className="absolute bottom-0 left-0 right-0 h-32"></div>
 						<div className="absolute right-30 mt-30">
 							<div className="flex flex-col items-end">
 								<h1 className="text-[96px] font-black text-white tracking-wider mb-2 title">Away</h1>
@@ -41,9 +43,10 @@ export default function App() {
 				</>
 			) : (
 				<>
+					<SilkBackground color="#0b0416" scale={1} noiseIntensity={1.3} speed={3} rotation={270} />
 					<div className="absolute inset-0 z-10 flex flex-col">
-            <Navigation onLogout={() => setShowHomeScreen(true)} />
-         			<Visualizer noteLines={noteLines} />
+						<Navigation onLogout={() => setShowHomeScreen(true)} />
+						<Visualizer noteLines={noteLines} />
 						<Piano pianoKeys={pianoKeys} showKeys={showKeys} onPlayNote={playNote} onStopNote={stopNote} />
 					</div>
 				</>
